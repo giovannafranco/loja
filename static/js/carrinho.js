6877f9da0ea8cb1a2efdd05e601a74088652bb3a
guicarvalho <giiovanna_franco@hotmail.com>
2015-06-29 01:02:10 -0300

    ajustes no template e entrada do usuário para quantidade

--- static/js/carrinho.js
+++ static/js/carrinho.js
@@ -8,19 +8,24 @@ function atualizarValorItemCarrinho(e) {
     var idProdutoSelecionado = e.target.id.split('-')[1];
     var quantidade = $('#quantidade-'+idProdutoSelecionado).val();
 
-    $.ajax({
-        url: 'atualizar/'+idProdutoSelecionado+'/'+quantidade+'/',
-        success:
-            function(data) {
-                $('#valorItem-'+idProdutoSelecionado).text(data['item_valor']);
-                $('#valorTotal').text(data['total']);
-                $('#blocoMensagem').empty();
-                $('#blocoMensagem').append('<p class="alert alert-info">Produto atualizado com sucesso!</p>');
-            },
-        error: 
-            function(textStatus, errorThrow) {
-                $('#blocoMensagem').empty();
-                $('#blocoMensagem').append('<p class="alert alert-danger">Quantidade indisponível.</p>');
-            }
-    });
+    if (quantidade < 1) {
+         $('#blocoMensagem').empty();
+        $('#blocoMensagem').append('<p class="alert alert-danger">Quantidade inválida, quantidade não pode ser '+ quantidade +'.</p>');
+    } else {
+        $.ajax({
+            url: 'atualizar/'+idProdutoSelecionado+'/'+quantidade+'/',
+            success:
+                function(data) {
+                    $('#valorItem-'+idProdutoSelecionado).text(data['item_valor']);
+                    $('#valorTotal').text(data['total']);
+                    $('#blocoMensagem').empty();
+                    $('#blocoMensagem').append('<p class="alert alert-info">Produto atualizado com sucesso!</p>');
+                },
+            error: 
+                function(textStatus, errorThrow) {
+                    $('#blocoMensagem').empty();
+                    $('#blocoMensagem').append('<p class="alert alert-danger">Quantidade indisponível.</p>');
+                }
+        });
+    }
 }
